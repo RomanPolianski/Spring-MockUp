f4da04132e6c32b10756ab40010444c7db0a4a84
Roman <rpol123453@gmail.com>
2022-03-06 20:47:38 +0300

    fix burger menu as js array

--- assets/components/renderNavItems.js
+++ assets/components/renderNavItems.js
@@ -1,30 +1,33 @@
 const ROOT_NAV_ITEMS = document.getElementsByClassName("header-options");
+const ROOT_NAV_BURGER_ITEMS = document.getElementsByClassName("header-options-mobile");
+
 
 (function navItemsRender() {
   let htmlElems = NAV_ITEMS.reduce((html, { title, submenu }) => {
     if (submenu.length !== 0) {
       html += `
           <li class = "nav-item">
-            <a href = "#" class="has-menu bold">${title} <i class="arrow arrow-down"></i></a>
+            <a href = "#" class="menu-link bold">${title}</a>
+            <span class="menu-arrow"></span>
             <ul class = "menu-sublist">
         `;
       submenu.forEach((item) => {
         if (item === "View all Projects") {
           html += `
               <li>
-                <a href = "#" class = "blue-link">${item}</a>
+                <a href = "#" class = "menu-sublink blue-link">${item}</a>
               </li>
             `;
         } else if (item === "Development Tools") {
           html += `
               <li class = "uppercase-text">
-                ${item}
+                <a href = "#" class = "menu-sublink thin">${item}</a>
               </li>
             `;
         } else {
           html += `
             <li>
-              <a href = "#">${item}</a>
+              <a href = "#"  class = "menu-sublink blue">${item}</a>
             </li>
             `;
         }
@@ -36,7 +39,7 @@ const ROOT_NAV_ITEMS = document.getElementsByClassName("header-options");
     } else {
       html += `
           <li>
-            <a href = "#" class="has-menu bold">${title}</a>
+            <a href = "#" class="menu-link bold">${title}</a>
           </li>
         `;
     }
@@ -48,4 +51,58 @@ const ROOT_NAV_ITEMS = document.getElementsByClassName("header-options");
   Array.from(ROOT_NAV_ITEMS).forEach((item) => {
     item.innerHTML = html;
   });
+  Array.from(ROOT_NAV_BURGER_ITEMS).forEach((item) => {
+    item.innerHTML = html;
+  });
 })();
+
+// (function navItemsBurgerRender() {
+//     let htmlElems = NAV_ITEMS.reduce((html, { title, submenu }) => {
+//       if (submenu.length !== 0) {
+//         html += `
+//             <li>
+//                 <a href="#" class="wrapper__menu-link">${title}</a>
+//             </li>
+//             <ul class="wrapper__menu-sublink>
+//           `;
+//         submenu.forEach((item) => {
+//           if (item === "View all Projects") {
+//             html += `
+//                 <li>
+//                   <a href = "#" class="wrapper__menu-link>${item}</a>
+//                 </li>
+//               `;
+//           } else if (item === "Development Tools") {
+//             html += `
+//                 <li class = "uppercase-text">
+//                   ${item}
+//                 </li>
+//               `;
+//           } else {
+//             html += `
+//               <li>
+//                 <a href = "#" >${item}</a>
+//               </li>
+//               `;
+//           }
+//         });
+//         html += `
+//                 </ul>
+//             </li>
+//           `;
+//       } else {
+//         html += `
+//             <li>
+//               <a href = "#" class="wrapper__menu-link">${title}</a>
+//             </li>
+//           `;
+//       }
+//       return html;
+//     }, ``);
+//     const html = `
+//           ${htmlElems}
+//         `;
+//     Array.from(ROOT_NAV_BURGER_ITEMS).forEach((item) => {
+//       item.innerHTML = html;
+//     });
+//   })();
